d49cdde43b3a1921db1c9c0a1359fe8cb2fe412e
Ilyas Adiyasa <ilyasadiyasa4@gmail.com>
2022-10-30 19:05:05 +0700

    add dark header for job-list implementation

---
+++ components/dark-header/index.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import Image from 'next/image';
+
+import ImageDecoration from '../../assets/image/pexels-sora-shimazaki-5668845 1.png';
+import ArrowDecoration from '../../assets/image/variant-1.png';
+
+import './style.scss';
+
+const DarkHeader: React.FC = () => {
+  return (
+    <div className="dark-header--container">
+      <div className="text--container">
+        <h2>Dicoding Jobs</h2>
+        <h1>
+          Temukan lowongan yang <br /> cocok dengan Anda
+        </h1>
+      </div>
+
+      <div className="header--decorator">
+        <div className="parent-decoration">
+          <Image src={ArrowDecoration} alt="Arrow Decoration" />
+          <div className="child-decoration">
+            <Image src={ImageDecoration} alt="Image Decoration" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default DarkHeader;
