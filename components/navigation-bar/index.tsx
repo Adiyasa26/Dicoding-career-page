2c102752bb194fadd9ac7efc583dfc00b5035715
Ilyas Adiyasa <ilyasadiyasa4@gmail.com>
2022-10-30 19:04:50 +0700

    add navigation implementation

--- components/navigation-bar/index.tsx
+++ components/navigation-bar/index.tsx
@@ -1,23 +1,30 @@
 'use-client';
 
+import React from 'react';
 import Link from 'next/link';
-
 import Image from 'next/image';
 
 import './style.scss';
 
-import DicodingLogo from '../../assets/image/Main_Logo Dicoding.png';
+import DicodingLogoDark from '../../assets/image/Main_Logo Dicoding.png';
+import DicodingLogoLight from '../../assets/image/Dicoding_Logo.png';
 import CooperLogo from '../../assets/image/job.png';
-import DownLogo from '../../assets/image/chevron.png';
+import DownLogoDark from '../../assets/image/chevron-dark-mode.png';
+import DownLogoLight from '../../assets/image/chevron-light-mode.png';
 import ProfilePictureDummyLogo from '../../assets/image/user.png';
-import NotificationLogo from '../../assets/image/notification.png';
+import NotificationLogoDark from '../../assets/image/notification-dark-mode.png';
+import NotificationLogoLight from '../../assets/image/notification-light-mode.png';
 
-const Navigation: React.FC = () => {
+const Navigation: React.FC<{ darkMode: boolean }> = ({ darkMode }) => {
   return (
-    <div className={'navigation--container'}>
+    <div className={`navigation--container ${darkMode ? '' : 'light'}`}>
       <div className="navigation--container_left">
         <div className="logo">
-          <Image src={DicodingLogo} alt={'Logo Dicoding'} priority={true} />
+          <Image
+            src={darkMode ? DicodingLogoDark : DicodingLogoLight}
+            alt={'Logo Dicoding'}
+            priority={true}
+          />
           <span>Jobs</span>
           <div className="line"></div>
         </div>
@@ -32,15 +39,24 @@ const Navigation: React.FC = () => {
         <div className="dropdown--container">
           <Image src={CooperLogo} alt="Logo Koper" />
           <span>Siap untuk di-interview</span>
-          <Image src={DownLogo} alt="Logo Anak Panah Turun" />
+          <Image
+            src={darkMode ? DownLogoDark : DownLogoLight}
+            alt="Logo Anak Panah Turun"
+          />
         </div>
 
         <div className="control-user--container">
           <div className="profile-picture--container">
             <Image src={ProfilePictureDummyLogo} alt="Logo Profile Picture" />
-            <Image src={DownLogo} alt="Logo Anak Panah Turun" />
+            <Image
+              src={darkMode ? DownLogoDark : DownLogoLight}
+              alt="Logo Anak Panah Turun"
+            />
           </div>
-          <Image src={NotificationLogo} alt="Logo Notifikasi" />
+          <Image
+            src={darkMode ? NotificationLogoDark : NotificationLogoLight}
+            alt="Logo Notifikasi"
+          />
         </div>
       </div>
     </div>
