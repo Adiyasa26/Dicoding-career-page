8f1fd492203dc8f27654ab756fcf2be459a155d4
Ilyas Adiyasa <ilyasadiyasa4@gmail.com>
2022-10-30 19:05:17 +0700

    create layout for job list page

---
+++ app/careers/job-list/layout.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import Navigation from '../../../components/navigation-bar';
+import DarkHeader from '../../../components/dark-header';
+
+import '../../globals.css';
+import Style from './job-list.module.scss';
+
+const JobListLayout: React.FC<{
+  children: React.ReactNode;
+  darkMode: boolean;
+}> = ({ children }) => {
+  return (
+    <div className={Style.container}>
+      <Navigation darkMode={true} />
+      <DarkHeader />
+      {children}
+    </div>
+  );
+};
+export default JobListLayout;
