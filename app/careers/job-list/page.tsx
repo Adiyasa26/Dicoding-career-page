eb25cff46ff48146dbb57972fe0b4279566a0739
Ilyas Adiyasa <ilyasadiyasa4@gmail.com>
2022-11-02 00:53:10 +0700

    move few component into client components

--- app/careers/job-list/page.tsx
+++ app/careers/job-list/page.tsx
@@ -1,10 +1,8 @@
 import React from 'react';
 import axios from 'axios';
 
-import JobListCard from '../../../components/jobList-card';
-
 import Style from './job-list.module.scss';
-import Pagination from '../../../components/Pagination';
+import JobListContainer from '../../../components/jobList-container';
 
 export async function getData(id = '') {
   const jobs = await axios.get(
@@ -14,40 +12,10 @@ export async function getData(id = '') {
   return jobs;
 }
 
-const JobListPage = async () => {
-  const jobs = await getData();
-
-  const pages = [
-    jobs.data.slice(0, 8),
-    jobs.data.slice(8, 16),
-    jobs.data.slice(16, 24),
-    jobs.data.slice(24, 32),
-    jobs.data.slice(32, 40),
-    jobs.data.slice(40, 43),
-  ];
-
-  console.log(pages[0]);
-
+const JobListPage = () => {
   return (
     <div className={Style.page}>
-      <div className={Style.container}>
-        <h1 className={Style.title}>Daftar Pekerjaan Terbaru</h1>
-        {pages[2].map((job: any) => (
-          <JobListCard
-            key={job.id}
-            id={job.id}
-            image={job.logo}
-            title={job.title}
-            companyName={job.company}
-            jobType={job.job_type}
-            location={job.location}
-            experience={job.experience}
-            createdAt={job.createdAt}
-            closedAt={job.closeAt}
-          />
-        ))}
-        <Pagination pages={pages} />
-      </div>
+      <JobListContainer />
     </div>
   );
 };
